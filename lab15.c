6f797423ff0a278eefc09fad937c5027e275c379
Bijay Shrestha <108262068+Bijay-Shre-stha@users.noreply.github.com>
2024-01-25 00:39:11 +0545

    lab report 4

--- lab15.c
+++ lab15.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 
-void hanoi(int n, char s, char d, char a) {
+void hanoi(int n, char s, char d, char a, int *step) {
     if (n) {
-        hanoi(n-1, s, a, d);
-        printf("Move disk %d from %c to %c\n", n, s, d);
-        hanoi(n-1, a, d, s);
+        hanoi(n-1, s, a, d, step);
+        (*step)++;
+        printf("Step %d: Move ring %d from %c to %c\n", *step, n, s, d);
+        hanoi(n-1, a, d, s, step);
     }
 }
 
@@ -12,6 +13,7 @@ int main() {
     int n;
     printf("Enter the number of rings: ");
     scanf("%d", &n);
-    hanoi(n, 'A', 'C', 'B');
+    int step = 0;
+    hanoi(n, 'A', 'C', 'B', &step);
     return 0;
 }

3447021b77b00bd33e9e0f98a9fb8c5e6e059d1d
Bijay Shrestha <108262068+Bijay-Shre-stha@users.noreply.github.com>
2024-01-24 21:56:42 +0545

    lab report 3

--- lab11.c
+++ lab11.c
@@ -2,36 +2,45 @@
 
 #define MAX_SIZE 3
 
-struct Queue {
+struct Queue
+{
     int items[MAX_SIZE];
     int front, rear;
 };
 
-void enqueue(struct Queue *q, int value) {
-    if (q->rear == MAX_SIZE - 1) {
-        printf("Queue is full. Cannot enqueue.\n");
+void enqueue(struct Queue *q, int value)
+{
+    if (q->rear == MAX_SIZE - 1)
+    {
+        printf("Queue is Overflow. %d Cannot enqueue.\n", value);
         return;
     }
 
     q->items[++q->rear] = value;
-    if (q->front == -1) q->front = 0;
+    if (q->front == -1)
+        q->front = 0;
 
     printf("Enqueued: %d\n", value);
 }
 
-void dequeue(struct Queue *q) {
-    if (q->front == -1) {
-        printf("Queue is empty. Cannot dequeue.\n");
+void dequeue(struct Queue *q)
+{
+    if (q->front == -1 || q->front > q->rear)
+    {
+        printf("Queue is Underflow. Cannot dequeue.\n");
         return;
     }
 
     printf("Dequeued: %d\n", q->items[q->front++]);
 
-    if (q->front > q->rear) q->front = q->rear = -1;
+    if (q->front > q->rear)
+        q->front = q->rear = -1;
 }
 
-void display(struct Queue *q) {
-    if (q->front == -1) {
+void display(struct Queue *q)
+{
+    if (q->front == -1)
+    {
         printf("Queue is empty.\n");
         return;
     }
@@ -42,15 +51,18 @@ void display(struct Queue *q) {
     printf("\n");
 }
 
-int main() {
+int main()
+{
     struct Queue myQueue = {{0}, -1, -1};
 
-    for (int i = 1; i <= MAX_SIZE+1 ; i++) enqueue(&myQueue, i * 10);
+    for (int i = 1; i <= MAX_SIZE; i++)
+        enqueue(&myQueue, i * 10);
     display(&myQueue);
 
-    enqueue(&myQueue, 100); // Overflow
+    enqueue(&myQueue, 40); // Overflow
 
-    for (int i = 0; i < MAX_SIZE+1 ; i++) dequeue(&myQueue); // Underflow
+    for (int i = 0; i < MAX_SIZE + 1; i++)
+        dequeue(&myQueue); // Underflow
     display(&myQueue);
 
     return 0;
